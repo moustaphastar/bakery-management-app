7947f10237e693c6dd79e1e5bf310687c4ac5aaf
moustaphastar <mustafayildiz1881@gmail.com>
2021-01-25 05:22:51 +0300

    Revert "Initial commit"
    This reverts commit 2bf02481

--- src/main/java/com/cayirozu/repository/MerchantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cayirozu.repository;
-
-import com.cayirozu.db.entity.Merchant;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MerchantRepository extends CrudRepository<Merchant, Integer> {
-}
