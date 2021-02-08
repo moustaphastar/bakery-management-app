ca10c40cd0059e80ae04ed91ecd8cbb500a52c33
moustaphastar <mustafayildiz1881@gmail.com>
2021-02-08 06:14:18 +0300

    Added package-info

---
+++ src/main/java/com/bakery/management/dao/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides the data access objects for domain model classes.
+ *
+ * @author Moustapha Star
+ * @version 1.0
+ * @since 1.0
+ */
+package com.bakery.management.dao;
---
+++ src/main/java/com/bakery/management/enums/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides the enum classes which holds the application and database wide
+ * enumeration constant values.
+ *
+ * @author Moustapha Star
+ * @version 1.0
+ * @since 1.0
+ */
+package com.bakery.management.enums;
