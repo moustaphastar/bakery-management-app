d64c63ad759c3aed33b575998c11ffe5bf94bb24
moustaphastar <38510483+moustaphastar@users.noreply.github.com>
2021-02-10 18:11:05 +0300

    Code review
    Removed unused imports.

--- src/test/java/com/bakery/management/BakeryTests.java
+++ src/test/java/com/bakery/management/BakeryTests.java
@@ -1,11 +1,8 @@
 package com.bakery.management;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
