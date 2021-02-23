5996593a0bc89b8ee38671da2f0c1bfa75d7e164
moustaphastar <mustafayildiz1881@gmail.com>
2021-02-23 17:15:28 +0300

    Refactored DateHelpers utility class

--- src/main/java/com/bakery/management/helpers/DateHelpers.java
+++ src/main/java/com/bakery/management/helpers/DateHelpers.java
@@ -1,11 +1,10 @@
 package com.bakery.management.helpers;
 
-import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.time.Period;
 import java.time.ZoneOffset;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /***
  * Provides helper methods for date operations.
@@ -15,113 +14,157 @@ import java.util.Map;
 public class DateHelpers {
 
     /***
+     * DateTimeFormatter with UTC ZoneOffset.
+     */
+    private static final DateTimeFormatter DATE_TIME_FORMATTER =
+            DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(ZoneOffset.UTC);
+
+    /***
      * Current date as LocalDate.
      */
-    // todo: LocalDateTime or LocalDate?
-    private static final LocalDate CURRENT_DATE = LocalDate.now();
+    private static final OffsetDateTime NOW = OffsetDateTime
+            .now(ZoneOffset.UTC);
+
+    /***
+     * Maximum distance allowed between two dates in total months.
+     * An application wide rule related to date and time actions.
+     */
+    private static final int MAX_MONTH_DISTANCE = 3;
+
+    /***
+     * Earliest allowed date and time with UTC offset backwards.
+     * First instant of the year which is 5 years before now.
+     * An application wide rule related to date and time actions.
+     */
+    private static final OffsetDateTime MIN_INSTANT =
+            OffsetDateTime.of(
+                    NOW.getYear() - 5,
+                    1, 1,
+                    0,
+                    0,
+                    0,
+                    0,
+                    ZoneOffset.UTC);
 
     /***
      * State of validity of date parameters.
      * {@see validatePeriod} for validation rules.
      */
-    private static boolean isPeriodValid = false;
+    private static boolean datesValidatedAndSorted = false;
 
     /***
-     * Holds beginning date.
+     * Holds earlier date.
      */
-    private final LocalDate dateFrom;
+    private OffsetDateTime earlierDate;
 
     /***
-     * Holds ending date.
+     * Holds later date.
      */
-    private final LocalDate dateTo;
+    private OffsetDateTime laterDate;
 
     /***
-     * Holds beginning and ending dates in order.
-     * First entry key is fromDate and second entry key is toDate.
+     * Constructor with two OffsetDateTime parameters.
+     * @param date1 the date that is believed to be earlier
+     * @param date2 the date that is believed to be later
      */
-    private final Map<String, LocalDate> orderedMap = new HashMap<>(2);
+    public DateHelpers(final OffsetDateTime date1,
+                       final OffsetDateTime date2) {
+        this.earlierDate = date1;
+        this.laterDate = date2;
+        validateAndSortDates();
+    }
 
     /***
-     * Constructor with two date parameters.
-     * @param dateOne the date that is believed to be earlier
-     * @param dateTwo the date that is believed to be later
+     * Constructor with two String parameters.
+     * @param date1 the date that is believed to be earlier
+     * @param date2 the date that is believed to be later
      */
-    public DateHelpers(final LocalDate dateOne, final LocalDate dateTwo) {
-        this.dateFrom = dateOne;
-        this.dateTo = dateTwo;
+    public DateHelpers(final String date1, final String date2)
+            throws DateTimeParseException, IllegalArgumentException {
+        this(OffsetDateTime.parse(date1, DATE_TIME_FORMATTER),
+                OffsetDateTime.parse(date2, DATE_TIME_FORMATTER));
+    }
+
+    /***
+     * Parses a ISO 8061 formatted String into OffsetDateTime with UTC offset.
+     * @param date ISO 8061 formatted date time with offset string
+     * @return parsed OffsetDateTime object
+     */
+    // todo: formatter utc is to format output or to catch utc in input string?
+    public static OffsetDateTime parse(final String date)
+            throws DateTimeParseException {
+        return OffsetDateTime.parse(date, DATE_TIME_FORMATTER);
     }
 
     /**
-     * Checks if given two dates match those three criteria.
-     * First: maximum distance allowed in total months between dates.
-     * Second: maximum distance backwards from now.
-     * Three: dates are before now.
-     *
-     * @param maxDistanceInMonths maximum distance allowed in total months
-     * @return this
+     * Checks if given two dates match three criteria.
+     * 1st: dates are before {@see NOW}
+     * 2nd: dates are after {@see MIN_INSTANT}
+     * 3rd: distance of dates in months is not gt {@see MAX_MONTH_DISTANCE}
      */
-    // todo: Add comparison for CURRENT_DATE.
-    public DateHelpers validatePeriod(final int maxDistanceInMonths) {
-        long period = Period.between(dateFrom, dateTo).toTotalMonths();
-
-        if (dateFrom.isBefore(CURRENT_DATE)
-                & dateTo.isBefore(CURRENT_DATE)
-                & period <= maxDistanceInMonths) {
-            isPeriodValid = true;
+    private void validateAndSortDates() throws IllegalArgumentException {
+        // 1st criteria check.
+        if (earlierDate.isAfter(NOW) || laterDate.isAfter(NOW)) {
+            throw new IllegalArgumentException(
+                    "A date cannot be later than now.");
         }
 
-        return this;
-    }
-
-    /**
-     * Builds a Map containing ordered result if time period is valid,
-     * otherwise returns null. fromDate is the earlier one. If dates
-     * are equal then fromDate parameter is accepted the earlier one.
-     *
-     * @return this
-     */
-    public DateHelpers buildMap() {
-        if (isPeriodValid) {
-            if (dateFrom.isBefore(dateTo)) {
-                orderedMap.put("fromDate", dateFrom);
-                orderedMap.put("toDate", dateTo);
-            } else {
-                orderedMap.put("fromDate", dateTo);
-                orderedMap.put("toDate", dateFrom);
-            }
+        // 2nd criteria check.
+        if (earlierDate.isBefore(MIN_INSTANT)
+                || laterDate.isBefore(MIN_INSTANT)) {
+            throw new IllegalArgumentException(
+                    "A date can't be before than " + MIN_INSTANT);
         }
 
-        return this;
+        // 3rd criteria check.
+        long period = Period
+                .between(earlierDate.toLocalDate(), laterDate.toLocalDate())
+                .toTotalMonths();
+
+        // If dates provides in reverse order,
+        if (period < 0) {
+            // Reverse the dates.
+            final OffsetDateTime tempDate = earlierDate;
+            earlierDate = laterDate;
+            laterDate = tempDate;
+            // Absolute the period value.
+            period = period * -1;
+        }
+
+        if (period < 0 || period > MAX_MONTH_DISTANCE) {
+            throw new IllegalArgumentException(
+                    "Distance between dates are gt "
+                            + MAX_MONTH_DISTANCE + " months");
+        }
+
+        datesValidatedAndSorted = true;
+
     }
 
     /**
-     * Returns the value with fromDate key from {@link #orderedMap}.
+     * Returns the earlier OffsetDateTime if dates are validated and sorted.
      *
-     * @return beginning date
+     * @return earlier date value or null
+     * @see #datesValidatedAndSorted
      */
-    public OffsetDateTime getFromDate() {
-        if (isPeriodValid) {
-            return orderedMap
-                    .get("fromDate")
-                    .atStartOfDay()
-                    .atOffset(ZoneOffset.UTC);
+    public OffsetDateTime getEarlierDate() {
+        if (datesValidatedAndSorted) {
+            return earlierDate;
         }
         return null;
     }
 
     /**
-     * Returns the value with toDate key from {@link #orderedMap}.
+     * Returns the earlier OffsetDateTime if dates are validated and sorted.
      *
-     * @return ending date
+     * @return later date value or null
+     * @see #laterDate
      */
-    public OffsetDateTime getToDate() {
-        if (isPeriodValid) {
-            return orderedMap
-                    .get("toDate")
-                    .atStartOfDay()
-                    .atOffset(ZoneOffset.UTC);
+    public OffsetDateTime getLaterDate() {
+        if (datesValidatedAndSorted) {
+            return laterDate;
         }
         return null;
     }
+
 }
