cdc43bdf6be4a7a9d55680aa7fcbf92427d02cbc
moustaphastar <mustafayildiz1881@gmail.com>
2021-04-21 02:15:19 +0300

    Remove unnecessary annotation

--- src/main/java/com/bakery/management/Application.java
+++ src/main/java/com/bakery/management/Application.java
@@ -3,11 +3,9 @@ package com.bakery.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication()
-@ComponentScan({"com.bakery.management"})
 @EntityScan("com.bakery.management.model")
 @EnableJpaRepositories("com.bakery.management.repository")
 public class Application {
